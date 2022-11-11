c9d6a5c6049d1bb379c8b30c096250cd4ac0b4ee
Mohamed Abdalrazek <91884590+lfg6f4w@users.noreply.github.com>
2022-11-11 21:14:15 +0200

    UVA 10617
    https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&category=0&problem=1558&mosmsg=Submission+received+with+ID+27995004

---
+++ lfg6f4w/dp_Again Palindrome.cpp
@@ -0,0 +1,69 @@
+/*** وَمَا تَوْفِيقِي إِلَّا بِاللَّهِ ۚ عَلَيْهِ تَوَكَّلْتُ وَإِلَيْهِ أُنِيبُ ***/
+
+#include                             <bits/stdc++.h>
+#define     ll                       long long
+#define     ld                       long double
+#define     ft                       first
+#define     sd                       second
+#define     ed                       '\n'
+#define     sz(x)                    x.size()
+#define     all(x)                   x.begin(), x.end()
+#define     rall(x)                  x.rbegin(), x.rend()
+#define     fixed(x)                 fixed<<setprecision(x)
+#define     memo(dp,x)               memset(dp,x,sizeof(dp))
+#define     mod                      1'000'000'007
+#define     OO                       2'000'000'000
+#define     EPS                      1e-7
+#define     pi                       acos(-1)
+#define     Good_Bay                 return 
+using namespace std;
+
+template < typename T = int > istream& operator >> (istream& in, vector < T >& v) {
+    for (auto& x : v) in >> x;
+    return in;
+}
+
+template < typename T = int > ostream& operator << (ostream& out, const vector < T >& v) {
+    for (const T& x : v) out << x << ' ';
+    return out;
+}
+void Accept() {
+    ios_base::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+int dx[] = { -1 ,  0 ,  1 , 1 , 1 , 0 , -1 , -1 };
+int dy[] = { -1 , -1 , -1 , 0 , 1 , 1 ,  1 ,  0 };
+string s;
+ll dp[100][100];
+ll AllPalindromes(int i, int j) {
+    if (i == j)Good_Bay 1;
+    if (i + 1 == j)Good_Bay 2 + (s[i] == s[j]);
+    ll& ret = dp[i][j];
+    if (~ret)Good_Bay ret;
+    ret = 0;
+    if (s[i] == s[j])
+        ret += AllPalindromes(i + 1, j - 1) + 1;
+    ret += AllPalindromes(i + 1, j);
+    ret += AllPalindromes(i, j - 1);
+    ret -= AllPalindromes(i + 1, j - 1);
+    Good_Bay ret;
+}
+void doWork(ll tt) {
+    cin >> s;
+    memo(dp, -1);
+    cout << AllPalindromes(0, sz(s) - 1);
+}
+int main() {
+    Accept();
+    int t = 1;
+    cin >> t;
+    for (int i = 1;i <= t;i++)
+        // cout << "Case "<<i<<":\n",
+        doWork(i), cout << ed;
+    Good_Bay 0;
+}
